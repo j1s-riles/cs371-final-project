4d8f774a7bb6e02c114a96ca7bb16fb2b96105bb
Dustin Thurston <dthurston1996@gmail.com>
2019-04-12 18:09:19 -0400

    Added login view

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import SignIn from './views/signIn.vue'
 
 Vue.use(Router)
 
@@ -10,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'sign-in',
+      component: SignIn
     },
     {
       path: '/about',
---
+++ src/views/signIn.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="top">
+        <h1>Login Page</h1>
+        <div id="loginForm">
+            <label for="usernameField">Username:</label>
+            <br>
+            <input type="email" id="usernameField">
+            <br>
+            <label for="passField">Password:</label>
+            <br>
+            <input type="password" id="passField">
+            <br>
+            <button v-on:click="authenticate">Login</button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'signIn',
+    data(){
+        return{}
+    }
+}
+
+</script>
+
