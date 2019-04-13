08cffb5ce492c10f78d97703cf0321c194f8d649
Dustin Thurston <dthurston1996@gmail.com>
2019-04-12 22:10:38 -0400

    cannot access app without authentication

--- src/router.js
+++ src/router.js
@@ -3,22 +3,34 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import SignIn from './views/signIn.vue'
 import SignUp from './views/SignUp.vue'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '*',
+      redirect: '/login'
+    },
+    {
       path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
       name: 'SignIn',
       component: SignIn
     },
     {
       path: '/dashboard',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/signup',
@@ -26,4 +38,17 @@ export default new Router({
       component: SignUp
     }
   ]
-})
+});
+
+//can't access application without authorization
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  console.log(currentUser);
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if(requiresAuth && !currentUser) next('login');
+  else if(!requiresAuth && currentUser) next('dashboard');
+  else next();
+});
+
+export default router;
